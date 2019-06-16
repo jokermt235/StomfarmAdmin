01ff2b19ad1a8c5d93f0229715566710c7c84ad8
mutalip@exesoft.org <m.usmanov@exesoft.org>
2019-06-16 22:47:16 +0000

    update all markwq

--- config/app.php
+++ config/app.php
@@ -256,7 +256,7 @@ return [
              */
             //'port' => 'non_standard_port_number',
             'username' => 'stomfarm',
-            'password' => 'Hr1Brb2a',
+            'password' => 'StomPass4u_',
             'database' => 'stomdb',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
@@ -384,4 +384,7 @@ return [
         'defaults' => 'php',
     ],
 
+    'OneC'=>[
+        'api_url'=>''
+    ],
 ];
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,24 +23,35 @@ class ComposerAutoloaderInitad40523251c63b9419083d24b3f85abf
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitad40523251c63b9419083d24b3f85abf', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInitad40523251c63b9419083d24b3f85abf::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitad40523251c63b9419083d24b3f85abf::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequiread40523251c63b9419083d24b3f85abf($fileIdentifier, $file);
         }
